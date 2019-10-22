f36567aee4cb5a3f3f696a0fa6754e577248bf19
Téo Le Dozo <teo@couaque.fr>
2019-10-22 23:00:50 +0100

    The generate feature is now working, but there might be some bugfixing to do

--- cchashlib.py
+++ cchashlib.py
@@ -1,10 +1,17 @@
 import sys
 
-def stringToIntArray(string):
+#This function is used to transform a string array (the one we get as an argument) into an integer array
+def stringToIntegerArray(inputString):
     res = []
-    for i in range(len(string)):
-        res.append(ord(string[i]) - 48)
+    #For each character in our input array, we get the character, transform it into an integer and remove 48, which is the offset of the numbers in the ASCII table.
+    for i in range(len(inputString)) :
+        res.append(ord(inputString[i]) - 48)
     return res
+
+#This function is used to transform a string array (the one we get as an argument) into an integer array
+def integerArrayToString(inputNumber):
+    print(chr(inputNumber))
+    return chr(inputNumber)
 
 def checkNumberIntegrity(string):
     if len(string) != 16 :
@@ -16,3 +23,10 @@ def checkNumberIntegrity(string):
         else:
             sys.exit("The credit card number you put is invalid !")
     return res
+
+
+def luhnAddition(inputInt):
+    if(inputInt > 4):
+        return 1 + (inputInt*2 % 10)
+    else:
+        return inputInt * 2
