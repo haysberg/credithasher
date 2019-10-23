87b081ebbb0f8bd2c78ae04cf5a178ee4388af83
Téo Le Dozo <teo@couaque.fr>
2019-10-23 12:34:21 +0100

    Finished documenting the other files. Bugfix will follow.

--- generate.py
+++ generate.py
@@ -5,6 +5,14 @@ import random
 import sys
 
 def generate(inputString):
+    """This function allows us to generate a valid credit card number from a string. It can take string from 1 to 15 characters long
+    
+    Arguments:
+        inputString {string} -- The credit card partial number that we have. It is always placed at the start
+    
+    Returns:
+        string -- The generated card number
+    """
     generatedLength = 16 - (len(inputString))
     inputString += str(random.randint(0, 10 ** (generatedLength - 1) - 1))
     print("The pre-generated number is : " + str(inputString))
