d280ccde9713fd09b92c038525c0c45a0a1a06ba
Téo Le Dozo <teo@couaque.fr>
2019-10-30 16:34:37 +0000

    Fixed a generation issue where we would get an exception when the generated number contained 0s at the start. 023 would become 23, so the number would not be long enough

--- cchashlib.py
+++ cchashlib.py
@@ -38,7 +38,7 @@ def checkNumberIntegrity(string):
         [ -- [description]
     """
     if len(string) != 16 :
-        sys.exit("The credit card number you put is not 16 numbers long !")
+        sys.exit("The credit card number you put is not 16 numbers long, it's " + str(len(string)))
     res = []
     for i in range(len(string)):
         if ord(string[i]) > 47 and ord(string[i]) < 58 :
--- generate.py
+++ generate.py
@@ -13,10 +13,23 @@ def generate(inputString):
     Returns:
         string -- The generated card number
     """
+    print("============================")
+    print("The given number is : " + inputString)
+    print("The number length is : " + str(len(inputString)))
+    
     generatedLength = 16 - (len(inputString))
-    inputString += str(random.randint(0, 10 ** (generatedLength - 1) - 1))
-    print("The pre-generated number is : " + str(inputString))
+    print("We're going to generate a number between 0 and " + str(10 ** generatedLength - 1))
+    genString = str(random.randint(10 ** (generatedLength -1 ), (10 ** generatedLength) - 1))
+    print("The generated following numbers are : " + genString)
+    inputString += genString
+    inputString = inputString[:15]
+    
+    print("We need to generate " + str(generatedLength) + " numbers")
+    print("The pre-generated number is : " + str(inputString) + " it is long of : " + str(len(str(inputString))))
     print("The incomplete sum is : " + str(verify(inputString)))
+
+    
+
     numberNeeded = 10 - (verify(inputString) % 10) % 10
     if(numberNeeded == 10):
         numberNeeded = 0
@@ -24,5 +37,6 @@ def generate(inputString):
     for i in range(10):
         if (luhnAddition(i) == numberNeeded):
             inputString += str(luhnAddition(i))
+            #if len(inputString) == 16 :
             return inputString
-    sys.exit('Error during number generation !')
+    raise RuntimeError('There was an error during credit card number generation !')
