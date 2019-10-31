bbe728383b6589e452facd8c4a4f46c0582af956
Téo Le Dozo <teo@couaque.fr>
2019-10-31 15:06:30 +0000

    Made the code more readable. Created the checksum feature. Integrated the checksum feature to the generate feature.

--- vendor.py
+++ vendor.py
@@ -9,10 +9,13 @@ def checkVendor(ccNumber):
     Arguments:
         ccNumber {string} -- The card number that the user wants to verify
     """
-    infos = json.loads(getVendorAPI(ccNumber))
-    print("The vendor of the card is " + infos["scheme"])
-    print("This card has been issued in : " + infos["country"]["alpha2"])
-    print("This card is a " + infos["brand"] + " card")
+    if getVendorAPI(ccNumber) != False :
+        infos = json.loads(getVendorAPI(ccNumber))
+        print("The vendor of the card is " + infos["scheme"])
+        print("This card has been issued in : " + infos["country"]["alpha2"])
+        print("This card is a " + infos["brand"] + " card")
+    else:
+        raise ValueError('The number provided did not link to any vendor.')
 
 def getVendorAPI(ccNumber):
     """This function handles the API call to binlist. This is not in the checkVendor function so that we can reuse it anywhere.
@@ -23,5 +26,9 @@ def getVendorAPI(ccNumber):
     Returns:
         json -- The json response that we get from the API
     """
+
     request = requests.get(url = "https://lookup.binlist.net/" + ccNumber[:8])
+    if request.status_code == 404 :
+        print('The number provided did not link to any vendor.')
+        return False
     return json.dumps(request.json())
